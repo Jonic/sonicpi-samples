8e531f43d8bf58e9c849d8be58b0a26fd31f4a0f
Jonic Linley <jonic@raspberrypi.org>
2017-11-19 18:41:38 +0000

    Created GA config and updated helper with console logs

--- src/helpers/Analytics.js
+++ src/helpers/Analytics.js
@@ -1,18 +1,26 @@
 import ReactGA from 'react-ga'
 
-ReactGA.initialize('UA-85981-10')
+import config from '../_config/analytics'
+
+ReactGA.initialize(config.id)
 
 const trackPageView = () => {
+  console.log('trackPageView') // eslint-disable-line no-console
+
   ReactGA.set({ page: window.location.pathname })
   ReactGA.pageview(window.location.pathname)
 }
 
 const trackPlay = sample => {
-  ReactGA.event({
+  const event = {
     category: 'Audio',
     action:   'Sample Played',
     label:    sample,
-  })
+  }
+
+  console.log('trackPlay', event) // eslint-disable-line no-console
+
+  ReactGA.event(event)
 }
 
 export { trackPageView, trackPlay }
