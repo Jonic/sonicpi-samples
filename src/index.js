7f8c96014e90f90a7430aee67feba84065029594
Jonic Linley <jonic@raspberrypi.org>
2017-11-19 19:51:49 +0000

    Prepped index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { trackPageView } from './helpers/Analytics'
+
+import './application.css'
 
 ReactDOM.render(<App />, document.getElementById('react-root'), trackPageView())
 registerServiceWorker()
