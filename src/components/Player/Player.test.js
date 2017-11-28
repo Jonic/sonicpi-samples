beffcab16d18e698f2f4bb41347f28b06142a6f6
Jonic Linley <jonic@raspberrypi.org>
2017-11-28 13:53:15 +0000

    Added tests for Player

--- src/components/Player/Player.test.js
+++ src/components/Player/Player.test.js
@@ -1,8 +1,11 @@
-import React from 'react'
 import { shallow } from 'enzyme'
+import categories from '../../_config/categories'
 import Player from './Player'
+import React from 'react'
+import samples from '../../_config/samples'
 
 const isLoading = false
+const toggleIsLoading = jest.fn()
 const updateIsLoading = jest.fn()
 
 let wrapper
@@ -12,6 +15,7 @@ describe('<Player />', () => {
     wrapper = shallow(
       <Player
         isLoading={isLoading}
+        toggleIsLoading={toggleIsLoading}
         updateIsLoading={updateIsLoading}
       />
     )
@@ -19,5 +23,50 @@ describe('<Player />', () => {
 
   it('renders without crashing', () => {
     expect(wrapper.find('.c-Player')).toBePresent()
+  })
+
+  describe('categorySelectorChangeHandler', () => {
+    describe('when user selects "all"', () => {
+      it('sets state correctly', () => {
+        let category = 'all'
+        let expectedCurrentCategory = categories[0]
+        let expectedCurrentSample = samples[0]
+        let expectedFilteredSamples = samples
+
+        wrapper.instance().categorySelectorChangeHandler(category)
+
+        let newCurrentCategory = wrapper.state().currentCategory
+        let newCurrentSample = wrapper.state().currentSample
+        let newFilteredSamples = wrapper.state().filteredSamples
+
+        expect(newCurrentCategory).toEqual(expectedCurrentCategory)
+        expect(newCurrentSample).toEqual(expectedCurrentSample)
+        expect(newFilteredSamples).toEqual(expectedFilteredSamples)
+      })
+    })
+
+    describe('when user selects "sn"', () => {
+      it('sets state correctly', () => {
+        let category = 'sn'
+        let filteredSamples = [
+          'sn_dolf',
+          'sn_dub',
+          'sn_zome',
+        ]
+        let expectedCurrentCategory = category
+        let expectedCurrentSample = filteredSamples[0]
+        let expectedFilteredSamples = filteredSamples
+
+        wrapper.instance().categorySelectorChangeHandler(category)
+
+        let newCurrentCategory = wrapper.state().currentCategory
+        let newCurrentSample = wrapper.state().currentSample
+        let newFilteredSamples = wrapper.state().filteredSamples
+
+        expect(newCurrentCategory).toEqual(expectedCurrentCategory)
+        expect(newCurrentSample).toEqual(expectedCurrentSample)
+        expect(newFilteredSamples).toEqual(expectedFilteredSamples)
+      })
+    })
   })
 })
