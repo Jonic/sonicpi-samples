0771ff6c1063bce31698e5aa9a968b2df8e2861c
Jonic Linley <jonic@raspberrypi.org>
2017-11-28 14:06:45 +0000

    Expanded Played tests

--- src/components/Player/Player.test.js
+++ src/components/Player/Player.test.js
@@ -21,51 +21,93 @@ describe('<Player />', () => {
     )
   })
 
-  it('renders without crashing', () => {
-    expect(wrapper.find('.c-Player')).toBePresent()
+  describe('Renders...', () => {
+    it('without crashing', () => {
+      expect(wrapper.find('.c-Player')).toBePresent()
+    })
   })
 
-  describe('categorySelectorChangeHandler', () => {
-    describe('when user selects "all"', () => {
-      it('sets state correctly', () => {
-        let category = 'all'
-        let expectedCurrentCategory = categories[0]
-        let expectedCurrentSample = samples[0]
-        let expectedFilteredSamples = samples
+  describe('Methods', () => {
+    describe('categorySelectorChangeHandler()', () => {
+      describe('when user selects "all"', () => {
+        it('sets state correctly', () => {
+          let category = 'all'
+          let expectedCurrentCategory = categories[0]
+          let expectedCurrentSample = samples[0]
+          let expectedFilteredSamples = samples
 
-        wrapper.instance().categorySelectorChangeHandler(category)
+          wrapper.instance().categorySelectorChangeHandler(category)
 
-        let newCurrentCategory = wrapper.state().currentCategory
-        let newCurrentSample = wrapper.state().currentSample
-        let newFilteredSamples = wrapper.state().filteredSamples
+          let newCurrentCategory = wrapper.state().currentCategory
+          let newCurrentSample = wrapper.state().currentSample
+          let newFilteredSamples = wrapper.state().filteredSamples
 
-        expect(newCurrentCategory).toEqual(expectedCurrentCategory)
-        expect(newCurrentSample).toEqual(expectedCurrentSample)
-        expect(newFilteredSamples).toEqual(expectedFilteredSamples)
+          expect(newCurrentCategory).toEqual(expectedCurrentCategory)
+          expect(newCurrentSample).toEqual(expectedCurrentSample)
+          expect(newFilteredSamples).toEqual(expectedFilteredSamples)
+        })
+      })
+
+      describe('when user selects "sn"', () => {
+        it('sets state correctly', () => {
+          let category = 'sn'
+          let filteredSamples = [
+            'sn_dolf',
+            'sn_dub',
+            'sn_zome',
+          ]
+          let expectedCurrentCategory = category
+          let expectedCurrentSample = filteredSamples[0]
+          let expectedFilteredSamples = filteredSamples
+
+          wrapper.instance().categorySelectorChangeHandler(category)
+
+          let newCurrentCategory = wrapper.state().currentCategory
+          let newCurrentSample = wrapper.state().currentSample
+          let newFilteredSamples = wrapper.state().filteredSamples
+
+          expect(newCurrentCategory).toEqual(expectedCurrentCategory)
+          expect(newCurrentSample).toEqual(expectedCurrentSample)
+          expect(newFilteredSamples).toEqual(expectedFilteredSamples)
+        })
       })
     })
 
-    describe('when user selects "sn"', () => {
+    describe('loopCheckboxClickHandler', () => {
+      it('toggles `isLooping` state value', () => {
+        let isLooping = wrapper.state().isLooping
+        wrapper.instance().loopCheckboxClickHandler()
+        expect(wrapper.state().isLooping).toEqual(!isLooping)
+      })
+    })
+
+    describe('playStopClickHandler', () => {
+      it('toggles `isPlaying` state value', () => {
+        let isPlaying = wrapper.state().isPlaying
+        wrapper.instance().playStopClickHandler()
+        expect(wrapper.state().isPlaying).toEqual(!isPlaying)
+      })
+    })
+
+    describe('sampleEndedHandler', () => {
+      it('sets `isPlaying` state value to `false`', () => {
+        wrapper.instance().sampleEndedHandler()
+        expect(wrapper.state().isPlaying).toBeFalsy()
+      })
+    })
+
+    describe('sampleSelectorChangeHandler', () => {
       it('sets state correctly', () => {
-        let category = 'sn'
-        let filteredSamples = [
-          'sn_dolf',
-          'sn_dub',
-          'sn_zome',
-        ]
-        let expectedCurrentCategory = category
-        let expectedCurrentSample = filteredSamples[0]
-        let expectedFilteredSamples = filteredSamples
+        let expectedCurrentSample = samples[0]
+        let expectedIsPlaying = false
 
-        wrapper.instance().categorySelectorChangeHandler(category)
+        wrapper.instance().sampleSelectorChangeHandler(expectedCurrentSample)
 
-        let newCurrentCategory = wrapper.state().currentCategory
-        let newCurrentSample = wrapper.state().currentSample
-        let newFilteredSamples = wrapper.state().filteredSamples
+        let currentSample = wrapper.state().currentSample
+        let isPlaying = wrapper.state().isPlaying
 
-        expect(newCurrentCategory).toEqual(expectedCurrentCategory)
-        expect(newCurrentSample).toEqual(expectedCurrentSample)
-        expect(newFilteredSamples).toEqual(expectedFilteredSamples)
+        expect(currentSample).toEqual(expectedCurrentSample)
+        expect(isPlaying).toEqual(expectedIsPlaying)
       })
     })
   })
